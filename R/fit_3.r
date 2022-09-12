c840c628c0bde44a5ec51651f6aaae71f59c9ecc
biabbiassago <bbrusco@uci.edu>
2022-09-12 08:42:31 -0700

    update notebooks

--- R/fit_3.r
+++ R/fit_3.r
@@ -1,20 +1,20 @@
-"Fit 2: linear trend and spatial random effects"
+"Model with spatial and random effects- AR 2 (same as FIT 1 but with AR2 instead of AR1"
 
 setwd("/global/project/projectdirs/m1532/Projects_MVP/geospatial/GeoSpatial_Model/spatial-prediction")
 library(tidyverse)
 library(sf)
 library(spdep)
 library(CARBayesST)
-
+library(caret)
 
 DATE <- Sys.Date()
 OUT_PATH <- paste0(
-    "outputs/model_results/fixedtime_model_",
+    "outputs/model_results/AR2model_",
     DATE,
     ".rds"
 )
 
-## 1. Loading dataset and geometriecorr_mat = cor(design_matrix)
+## 1. Loading dataset and geometries ###
 vars <- read_csv("data/processed/combined.csv")
 # you are considering in your dataset.
 NUMBER_OF_YEARS <- length(unique(vars$year))
@@ -36,9 +36,10 @@ df_nona <- df %>% na.omit(suicide_rate) %>%
   ungroup() %>% 
   filter(name_count == NUMBER_OF_YEARS) %>% 
   dplyr::select(-name_count) %>%
-  arrange(desc(year), FIPSCODE) %>% as_tibble()
+  arrange(desc(year), FIPSCODE) %>% as_tibble() 
 
 #length(fips_to_keep)
+#1931
 
 ## 2. Creating the neighbours matrix. See file create_neighbours_matrix.R ###
 
@@ -49,22 +50,33 @@ neighbours_matrix <- readRDS(
     file = "data/processed/neighbours_matrix.rds"
 )
 
+
 vars_to_remove <- c(
     "STATEFP", "COUNTYFP", "TRACTCE", "AFFGEOID",
     "GEOID", "NAME", "NAMELSAD", "STUSPS", "NAMELSADCO",
-    "STATE_NAME", "LSAD", "geometry", "FIPSCODE", "county", "year", "deaths","pop"
-) # year incorporated by the ST.CARlinear model
+    "STATE_NAME", "LSAD", "geometry", "FIPSCODE", "county",
+    "year","deaths","pop"
+)
+
+
+
+# vars_to_remove <- c(
+#    "STATEFP", "COUNTYFP", "TRACTCE", "AFFGEOID",
+#    "GEOID", "NAME", "NAMELSAD", "STUSPS", "NAMELSADCO",
+#    "STATE_NAME", "LSAD", "geometry", "FIPSCODE", "county",
+#    "crude_rate", "adj_rate","suicide_rate","year"
+# ) 
+
+
+#df_nona[df_nona$year == 2018, "deaths"] = NA
+
 corr_mat = df_nona %>%
-    dplyr::select(-all_of(vars_to_remove)) %>% cor()
+    dplyr::select(-all_of(c(vars_to_remove))) %>% cor()
 
 hc = caret::findCorrelation(corr_mat, cutoff=0.99) 
 hc = sort(hc)
+# ACS_PCT_RENTED_HH	ACS_PCT_TRICARE_VA	ACS_TOTAL_HOUSEHOLD
 hc_names <- colnames(corr_mat[hc,hc])[-1]
-
-design_matrix <- df_nona %>%
-    dplyr::select(-all_of(vars_to_remove)) %>%
-    dplyr::select(-all_of(hc))
-
 
 predictors  <- setdiff(colnames(df_nona), c(vars_to_remove,hc_names))
 
@@ -77,17 +89,18 @@ model_formula <- as.formula(
     )
 )
 
-model <- ST.CARlinear(
+
+model <- ST.CARar(
     model_formula,
-    data=df_nona,
+    df_nona,
     family="poisson",
+    AR=2,
     W=neighbours_matrix,
     burnin=10000,
     n.sample=80000
 )
 
 saveRDS(model, OUT_PATH)
-
 
 # for prediction: set the outcome variable to NAN (deaths)
 # for these NAN values we get the posterior distributions via
--- sbatch_files/fit_3_run.sh
+++ sbatch_files/fit_3_run.sh
@@ -3,17 +3,12 @@
 #SBATCH -N 1
 #SBATCH -C knl
 #SBATCH --qos=regular
-#SBATCH -J fit2
+#SBATCH -J fit3
 #SBATCH --mail-user=bbrusco@lbl.gov
 #SBATCH --mail-type=ALL
-#SBATCH -t 16:00:00
-
-#OpenMP settings:
-export OMP_NUM_THREADS=1
-export OMP_PLACES=threads
-export OMP_PROC_BIND=true
+#SBATCH -t 18:30:00
 
 module load R
-source activate r-venv
 cd /global/project/projectdirs/m1532/Projects_MVP/geospatial/GeoSpatial_Model/spatial-prediction
-R CMD BATCH R/fit_2.r
+source activate r-venv
+R CMD BATCH R/fit_3.r
